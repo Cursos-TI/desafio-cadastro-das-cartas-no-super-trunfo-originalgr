24de5d51e8503008f991f798d9bd9c67a4ce35fc
originalgr <gustavorodriguescaldas@outlook.com>
2025-04-29 18:11:21 -0400

    Correções

--- AVENTUREIROCartasSuperTrunfo.c
+++ AVENTUREIROCartasSuperTrunfo.c
@@ -66,7 +66,7 @@ densidade1 = populacao1 / area1;
 pibPercapita1 = (pib1 * 1000000000) / populacao1; //Convertendo PIB de bilhoes para reais
 
 densidade2 = populacao2 / area2;
-pibPercapita2 = (pib1 * 1000000000) / populacao2;
+pibPercapita2 = (pib2 * 1000000000) / populacao2;
 
 
 
@@ -77,11 +77,11 @@ pibPercapita2 = (pib1 * 1000000000) / populacao2;
  printf("Código: %s\n",codigo1);
  printf("Nome da Cidade: %s\n",cidade1);
  printf("População: %d\n",populacao1);
- printf("Área: %.2f bilhões de reais\n",area1);
+ printf("Área: %.2f km²\n",area1);
  printf("PIB: %.2f bilhões de reais\n", pib1);
  printf("Número de Pontos Turísticos: %d\n",pontos1);
  printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
- printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
+ printf("PIB per Capita: %.2f reais\n", pibPercapita1);
  
  printf("\nCarta 2:\n");
  printf("Estado: %c\n", estado2);
@@ -92,7 +92,7 @@ pibPercapita2 = (pib1 * 1000000000) / populacao2;
  printf("PIB: %.2f bilhões de reais\n", pib2);
  printf("Número de Pontos Turísticos: %d\n", pontos2);
  printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
- printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+ printf("PIB per Capita: %.2f reais\n", pibPercapita2);
 
  return 0;
 
